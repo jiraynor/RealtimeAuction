03e40c66247b03beb7b5b59a8a699a6556dc5457
ByeonSeongUk <seonguk092@gmail.com>
2022-05-15 22:38:00 +0900

    회원가입, 로그인, 중복확인

--- server/src/app-data-source.ts
+++ server/src/app-data-source.ts
@@ -12,7 +12,7 @@ const AppDataSource = new DataSource({
   password: '6546',
   database: 'auction',
   entities: [Member, Auction_item, Item_img, Bid_log],
-  synchronize: true,
+  synchronize: false,
   logging: true,
 });
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,16 @@ import express, { Express } from 'express';
 import { auctionRouter, bidRouter, memberRouter } from './routers';
 import { DataSource } from 'typeorm';
 import AppDataSource from './app-data-source';
+import cookieParser from 'cookie-parser';
 
 // DB 연결
 const datasource: DataSource = AppDataSource;
 
 const app: Express = express(),
   port: number = 4000;
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auction', auctionRouter(datasource));
 app.use('/api/bid', bidRouter(datasource));
