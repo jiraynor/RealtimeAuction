b82835ebc08876a1e225a2ba62c4570e3c1ff8ab
jiraynor <lacls159@naver.com>
2022-05-15 10:50:47 +0900

    member model 작성

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,8 +1,11 @@
 import { Sequelize } from 'sequelize';
+import Member from './member';
 
 const sequelize = new Sequelize('auction', 'developer', 'qwer1234', {
   host: 'localhost',
   dialect: 'mysql',
 });
+
+console.log(Member(sequelize));
 
 export default sequelize;
---
+++ server/src/models/member.ts
@@ -0,0 +1,46 @@
+import { Sequelize, DataType, Model, DataTypes } from 'sequelize';
+
+const Member = (sequelize) => {
+  const member = sequelize.define('Member', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    tel: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    balance: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    account_num: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bank_code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
+
+export default Member;
