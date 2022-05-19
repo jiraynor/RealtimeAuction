a4c52b75b9e62ec9914c3716bd18179e5e8af856
ByeonSeongUk <seonguk092@gmail.com>
2022-05-19 18:34:54 +0900

    byeon

--- server/src/routers/bidRouter.ts
+++ server/src/routers/bidRouter.ts
@@ -23,28 +23,22 @@ router.post('/set', async (req: Request, res: Response, next: NextFunction) => {
   if (!id) res.status(401).send('권한없음');
 
   try {
-    // 아이디 가져오기
     const bider = await MemberRepository.findOneBy({ id });
-    // 상품 가져오기
     const auction_item = await AuctionRepository.findOneBy({ auction_num });
 
-    // 가격 오버 검증
     if (
       auction_item.current_price >= bid_price ||
       auction_item.appraisal_value >= bid_price
     )
       res.status(400).send('금액오류');
 
-    // 본인상품 막기
     if (id === auction_item.saler.id) res.status(400).send('본인상품');
 
-    // auction_item.current_price update
     await AuctionRepository.updateCurrentPrice(
       auction_item,
       parseInt(bid_price)
     );
 
-    // bid_log 넣기
     const bid = await BidRepository.set(
       auction_item,
       bider,
@@ -72,9 +66,6 @@ router.post(
 
       if (id === auction_item.saler.id) res.status(400).send('본인상품');
 
-      console.log(auction_item.auction_status);
-      console.log(auction_item.successful_bid_status);
-
       if (
         auction_item.auction_status === false ||
         auction_item.successful_bid_status === false
@@ -94,27 +85,21 @@ router.post(
 );
 
 // getBids: 입찰 리스트 보기
-router.get(
-  '/getBids',
-  async (req: Request, res: Response, next: NextFunction) => {
-    // req : auction_num
-    // res : log_num, auction_num, bider, bid_price, bid_datetime
+router.get('/getBids', async (req: Request, res: Response) => {
+  const id = auth(req.headers.authorization);
 
-    const id = auth(req.headers.authorization);
+  if (!id) res.status(401).send('권한없음');
 
-    if (!id) res.status(401).send('권한없음');
+  try {
+    const bider = await MemberRepository.findOneBy({ id });
 
-    try {
-      const bider = await MemberRepository.findOneBy({ id });
+    const bidList = await BidRepository.getBids(bider);
 
-      const bidList = await BidRepository.getBids(bider);
-      console.log(bidList);
-      res.status(200).json(bidList);
-    } catch (e) {
-      console.error(e.message);
-      res.status(503).end('실패');
-    }
+    res.status(200).json(bidList);
+  } catch (e) {
+    console.error(e.message);
+    res.status(503).end('실패');
   }
-);
+});
 
 export default router;
