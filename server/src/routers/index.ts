b6c36b7b6a95d7fd2fa957a28cf4ec911f331717
jiraynor <lacls159@naver.com>
2022-05-21 15:53:29 +0900

    return 추가

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -1,5 +1,4 @@
 import auctionRouter from './auction.router';
-import bidRouter from './bid.router';
 import memberRouter from './member.router';
 
-export { bidRouter, memberRouter, auctionRouter };
+export { memberRouter, auctionRouter };
